fdf1ae6eef0d7922ac5ec528255f6f643e0fd98e
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-01-25 14:43:40 +0200

    Added the componentss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
+import Header from "./Component/Header";
+import Balance from "./Component/Balance";
+import IncomeExpense from "./Component/IncomeExpense";
+import TransactionList from "./Component/TransactionList";
+import AddTransaction from "./Component/AddTransaction";
+import './App.css';
 function App() {
   return (
-    <>
-    </>
+    <div>
+    <Header/>
+    <div className="container">
+    <Balance/>
+    <IncomeExpense/>
+    <TransactionList/>
+    <AddTransaction/>
+    
+    </div>
+    </div>
   );
 }
 
